39a4510d5f02c72135f0a48888a96a678668ff99
Dynara <outdoorsy02@gmail.com>
2022-10-06 11:49:53 -0600

    group work

--- game/cards.py
+++ game/cards.py
@@ -8,7 +8,6 @@ class Card:
     Responsibility: to hold and display card information
 
     Attributes:
-    
     """
 
 # 2) Create the class constructor. Use the following method comment.
@@ -19,36 +18,9 @@ class Card:
             self (cards): An instance of cards
         """
         self.value = 0
-        # Game starts with 300 points
-        self.points = 300
+        
 
-# 3) Angela Display int between 1 to 13
+# 3) Display int between 1 to 13
     def display_card(self):
         self.value = random.randint(1, 13)
-
-# 4) Angela Player guess higher or lower
-#    def ask_player():
-#        pass
-
-# 5) Francisco Display amount earned or lost +100 points if correct, -75 if incorrect
-#    def earned_lost():
-#        pass
-
-# 6) Franciso Keep overall score, game started at 300, ends if player reaches 0
-#    def overall_score():
-#        pass
-
-# 7) Dylan Ask playing to keep playing if points > 0
-"""
-    def keep_playing():
-        print('Do you wish to continue play:')
-        x = input()
-        if x == 'Y' and points > 0:
-            display_card()
-            ask_player()
-            earned_lost()
-            overall_score()
-        elif x == 'N' or points == 0:
-            print('Thank you for playing')
-"""
-
+        return self.value
--- game/director.py
+++ game/director.py
@@ -1,6 +1,5 @@
-from functools import total_ordering
-from optparse import Values
-from game.cards import Card
+
+from cards import Card
 
 class Director:
     """A person who directs the game. 
@@ -11,19 +10,25 @@ class Director:
 
         
     """
-
+    # Angela
     def __init__(self):
         """Constructs a new Director.
         
         Args:
             
-        """
-        card = Card()
-        self.card = []
+        """    
+        
+        # Create empty list to populate from class Card  
+        self.playing_cards = []
         self.is_playing = True
-        self.score = 0
+        self.points = 0
         # Start Game with 300 for score
         self.total_score = 300
+
+        # Making list of random card
+        for i in range(2):
+            card = Card()
+            self.playing_cards.append(card)
 
     def start_game(self):
         """Starts the game by running the main game loop.
@@ -41,52 +46,60 @@ class Director:
     def get_inputs(self):
         """Ask the user if the card is hi or lo
 
-        Args:
-           self (Director): An instance of Director 
         """
-        guess_card = input("higher or lower? [h/l] ").lower()
-        if guess_card == "h" or guess_card == "l":
-            print("Next card was")
 
-
+        x = input('Do you wish to continue play: (y/n)').lower()
         
-# Stacie       
-    def do_updates(self):
-        """Updates the player's overall score
+        """
+        if x == 'y' and self.points > 0:
+            display_card()
+            ask_player()
+            earned_lost()
+            overall_score()
+        elif x == 'n' or points == 0:
+            print('Thank you for playing')
+        """
 
-        Args:
-            
+# Francisco & Oghenekome       
+    def do_updates(self):
+        """Updates the player's overall score   
+        The player earns 100 points if they guessed correctly.
+        The player loses 75 points if they guessed incorrectly.
+        If a player reaches 0 points the game is over        
         """
         if not self.is_playing:
             return  
+        
+        for i in range(len(self.playing_cards)):
+            new_card = self.playing_cards[i]
+            new_card.display_card()
+        
+        # Update total score
+        self.total_score += self.points
 
-        card.keep_playing()
-      
-        # Update score from the cards.earned_lost
-        # score = cards.earned_lost()
-
-        # Add the amount from earned_lost if positive
-        # if self.cards.value > 0:        
-        #   self.total_score += self.card.value
-        # Subtract amount lost if negative
-        # else:
-        #   self.total_score -= self.card.value
-         
         
 # Stacie
     def do_outputs(self):
-        """Displays . Also asks the player if they would like to keep playing
-
-        Args:
-            
+        """Displays next card and score
         """
         if not self.is_playing:
             return  
 
         # update value to what the next card is
-        values = card.display_card()
+        # value = card.display_card(self)
        
+        values = []
+        for i in range(len(self.playing_cards)):
+            new_card = self.playing_cards[i]
+            values += f"{new_card.value} "
+
+        print(f"Next card was: {values[1]}")
+        print(f"Your score is: {self.total_score}\n")
+        self.is_playing == (self.total_score > 0)
+
+        """
         # Display next card
-        print(f"Next card was: {values}")
+        print(f"Next card was: {value}")
         # Display total score
-        print(f"Your score is: {card.overall_score}\n")
+        print(f"Your score is: {total_score}\n")
+        """
